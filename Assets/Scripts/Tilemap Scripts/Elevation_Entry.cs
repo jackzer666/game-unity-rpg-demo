464c535ee0073329369cf871cb8ccebade93ec1d
jackzer <838735010@qq.com>
2026-06-15 20:06:38 +0800

    feat: 碰撞器实现山体高度

---
+++ Assets/Scripts/Tilemap Scripts/Elevation_Entry.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Collider2D[] mountainColliders;
+    public Collider2D[] boundaryColliders;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (Collider2D mountain in mountainColliders)
+            {
+                // 每一个碰撞体失效
+                mountain.enabled = false;
+            }
+
+            foreach (Collider2D boundary in boundaryColliders)
+            {
+                // 边界生效 避免玩家从山脉边缘直接走出去
+                boundary.enabled = true;
+            }
+
+            // 玩家碰撞体渲染到一个新的涂层？玩家层级更高
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
