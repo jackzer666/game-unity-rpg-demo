ffc31ff14c7d4693fa21815e015bf4e8dd1177f7
jackzer <838735010@qq.com>
2026-06-16 15:05:41 +0800

    feat: 血量显示与动画

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
 
+    public TMP_Text healthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        ChangeHPText();
+    }
+
+    private void ChangeHPText()
+    {
+        healthTextAnim.Play("TextUpdate");
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
+
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
-
+        ChangeHPText();
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
